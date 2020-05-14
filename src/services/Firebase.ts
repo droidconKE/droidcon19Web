06a05d4ba18a782d74d58f8ecd267a41f32a67dd
manuelgeek <emashmagak@appslab.co.ke>
2020-05-14 21:58:47 +0300

    fixes on firebase

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,20 +1,20 @@
 // import * as firebase from "firebase";
-import * as firebase from 'firebase/app'
-import 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/messaging'
+import * as firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/messaging';
 
-var config = {
-    
+let config = {
+
     apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-    authDomain: "droidconke-70d60.firebaseapp.com",
-    databaseURL: "https://droidconke-70d60.firebaseio.com",
-    projectId: "droidconke-70d60",
-    storageBucket: "droidconke-70d60.appspot.com",
+    authDomain: 'droidconke-70d60.firebaseapp.com',
+    databaseURL: 'https://droidconke-70d60.firebaseio.com',
+    projectId: 'droidconke-70d60',
+    storageBucket: 'droidconke-70d60.appspot.com',
     messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_ID,
 
   }; // 4. Get Firebase Configuration
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export default firebase
+export default firebase;
--- src/services/Messaging.ts
+++ src/services/Messaging.ts
@@ -1,91 +1,90 @@
 import Axios from 'axios';
 
-export function messaging (firebase: any){
-    let messo = null
-    if(firebase.messaging.isSupported()){
+export function messaging(firebase: any) {
+    const messo = null;
+    if (firebase.messaging.isSupported()) {
         console.log('Messaging Supported');
         const messaging = firebase.messaging();
-  
+
         messaging.usePublicVapidKey(process.env.VUE_APP_FIREBASE_PUBLIC_KEY); // 1. Generate a new key pair
-        
+
         // Request Permission of Notifications
         messaging.requestPermission().then(() => {
             console.log('Notification permission granted.');
             // console.log(process.env.VUE_APP_MESSAGING_TOPIC)
-        
+
             // Get Token
             messaging.getToken().then((token: any) => {
             //   console.log(token)
             sendTokenToServer(token);
-            })
-        }).catch((err:[]) => {
+            });
+        }).catch((err: []) => {
             console.log('Unable to get permission to notify.', err);
         });
 
         // Callback fired if Instance ID token is updated.
         messaging.onTokenRefresh(function() {
             messaging.getToken()
-            .then(function(refreshedToken:any) {
+            .then(function(refreshedToken: any) {
             console.log('Token refreshed.');
-            // Indicate that the new Instance ID token has not yet been sent 
+            // Indicate that the new Instance ID token has not yet been sent
             // to the app server.
             setTokenSentToServer(false);
             // Send Instance ID token to app server.
             sendTokenToServer(refreshedToken);
             })
-            .catch(function(err:[]) {
+            .catch(function(err: []) {
             console.log('Unable to retrieve refreshed token ', err);
             setTokenSentToServer(false);
             });
         });
-        
+
         // Send the Instance ID token your application server, so that it can:
         // - send messages back to this app
         // - subscribe/unsubscribe the token from topics
-        function sendTokenToServer(currentToken:any) {
+        function sendTokenToServer(currentToken: any) {
             if (!isTokenSentToServer()) {
             console.log('Sending token to server...');
             // TODO(developer): Send the current token to your server.
-            setTokenToTopic(currentToken)
+            setTokenToTopic(currentToken);
             setTokenSentToServer(true);
             } else {
             console.log('Token already sent to server so won\'t send it again ' +
                 'unless it changes');
             }
         }
-        
+
         function isTokenSentToServer() {
             return window.localStorage.getItem('sentToServer1') === '1';
         }
-        
+
         function setTokenSentToServer(sent: boolean) {
             window.localStorage.setItem('sentToServer1', sent ? '1' : '0');
         }
 
-            
+
             // Callback fired if Instance ID token is updated.
         messaging.onTokenRefresh(function() {
             messaging.getToken()
             .then(function(refreshedToken: any) {
             console.log('Token refreshed.');
-            // Indicate that the new Instance ID token has not yet been sent 
+            // Indicate that the new Instance ID token has not yet been sent
             })
-            .catch(function(err:[]) {
+            .catch(function(err: []) {
             console.log('Unable to retrieve refreshed token ', err);
             });
         });
 
 
-        function setTokenToTopic(token: string)
-        {
+        function setTokenToTopic(token: string) {
             //   console.log(process.env.VUE_APP_SERVER_KEY)
             const headers = {
-                'Authorization': 'key='+process.env.VUE_APP_SERVER_KEY,
-                'Content-Type' : 'application/json'
+                'Authorization': 'key=' + process.env.VUE_APP_SERVER_KEY,
+                'Content-Type' : 'application/json',
             };
-            let topic = process.env.VUE_APP_MESSAGING_TOPIC
-            Axios.post('https://iid.googleapis.com/iid/v1/'+token+'/rel/topics/'+topic,null,{headers})
-            .then(response => {
+            const topic = process.env.VUE_APP_MESSAGING_TOPIC;
+            Axios.post('https://iid.googleapis.com/iid/v1/' + token + '/rel/topics/' + topic, null, {headers})
+            .then((response) => {
                 //   console.log('Subscribed to "'+topic+'"');
                 console.log('Subscribed.');
                 //   console.log(response)
@@ -94,8 +93,8 @@ export function messaging (firebase: any){
                 // console.log(err.response.data)
                 });
         }
-    }else {
-        console.log('Not Supported')
+    } else {
+        console.log('Not Supported');
     }
-     
+
 }
