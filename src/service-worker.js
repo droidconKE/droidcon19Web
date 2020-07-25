f2cdc9cd6755be9e3f1261b12ffc0e276217ddaf
manuelgeek <emashmagak@appslab.co.ke>
2020-07-25 20:50:47 +0300

    updated version

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 workbox.core.setCacheNameDetails({ prefix: 'd4' })
 //Change this value every time before you build
-const LATEST_VERSION = 'v1.0'
+const LATEST_VERSION = 'v1.0.3'
 self.addEventListener('activate', (event) => {
   console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
   if (caches) {
